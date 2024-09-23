751b7d5661c919024c33a43d4302b386be30a048
Meng <zhangm19@cardiff.ac.uk>
2024-09-23 14:20:55 +0100

    completed all nimats

--- makeFile nimats.R
+++ makeFile nimats.R
@@ -35,9 +35,9 @@ take_sample <-
   }
 
 
-## nimat-main ---------------------------------------------------------
+## nimats-main ---------------------------------------------------------
 
-nimat_main_df <-
+nimats_main_df <-
   master_id_df %>%
   mutate(
     birth_ward = take_area(wards = T),# Birth ward code 1992 (M)
@@ -57,26 +57,121 @@ nimat_main_df <-
     
   )
 
-nimat_main_df
 
 ## nimats-obsteric (previous obsteric history) -------------------------
 
+nimats_obst_df <-
+  master_id_df %>%
+  mutate(
+    whooley_1 = c(T,F) %>% take_sample(), #Whooley Mood question 1 (Mood Q1)
+    whooley_2 = c(T,F) %>% take_sample(), #Whooley Mood question 2 (Mood Q2)
+    prev_ment_disorder_m = c(T,F) %>% take_sample(), #Previous mental health and mother reported disorders 
+    birth_this_n = 1:2 %>% take_sample(), #Births this pregnancy
+    sw_at_preg = c(T,F) %>% take_sample(), #Social worker at pregnancy (changed to social services in 2017)
+    smoke_preg_m = c(T,F) %>% take_sample(),#Smoking (mother)
+    smoke_preg_p = c(T,F) %>% take_sample(), #Smoking (partner)
+    alc_preg_m = 0:4 %>% take_sample() #alcohol (mother, units drunk)
+    
+  ) 
 
 
 # nimats-infant -----------------------------------------------------------
+nimats_infant_df <-
+  master_id_df %>%
+  mutate(
+    apgar_1 = 6:10 %>% take_sample(), # APGAR score at 1 min
+    apgar_5 = 6:10 %>% take_sample(),# APGAR score at 5 min
+    birth_live = c(T,F) %>% take_sample(), # Birth status (live or still)
+    birth_wgt = 1:5 %>% take_sample(), # Birth weight
+    birth_centile = 1:100 %>% take_sample(),# Centile (assessment of infant weight relative to gestational age)
+    bfeed_birth = c(T, F) %>% take_sample(),# Breastfeeding attempted (replaced by infant feeding Q later)
+    deliv_meth = letters[1:4] %>% take_sample(),# Delivery method
+    headcirum_i = 44:57 %>% take_sample(),# Head circumference (infant head circumference in cm)
+    length_i = 19:21 %>% take_sample(), # Infant length
+    sex_male_i = c(T,F) %>% take_sample()# Infant sex 
+    
+  )
 
 
-# nimats-breastfeed -------------------------------------------------------
-
+# nimats-bfeed -------------------------------------------------------
+nimats_bfeed <-
+  master_id_df %>%
+  mutate(
+    bfeed_meth = letters[1:6] %>% take_sample(),#Feeding Method (Description of feeding method used during hospital stay)
+    bfeed_cd = letters[1:6] %>% take_sample(), #Feeding Code
+    bfeed_attempt = c(T, F) %>% take_sample(), #- Breast Feeding Attempted (This object identifies whether or not the mother attempted to breastfeed during the stay in hospital (Y/N)):
+    bfeed_help_home = c(T, F) %>% take_sample()#  Help at home (This object indicates if the mother has been offered support to ensure that she can access help when at home (Y/N))
+    
+  )
 
 # nimats-delivery ---------------------------------------------------------
-
+nimats_delivery <-
+  master_id_df %>%
+  mutate(
+    bmi_deliv_m = c(18:30, NA) %>% take_sample() #Mother's BMI at delivery
+  )
 
 # nimats-parenting --------------------------------------------------------
 
+nimats_parenting <-
+  master_id_df %>%
+  mutate(
+    comm_baby_parent = letters[1:3] %>% take_sample(),# Communicate with baby
+    resp_baby_parent = letters[1:3] %>% take_sample(),# Responding to baby
+    skin2skin_birth = letters[1:3] %>% take_sample(), # Skin to skin/ skin to skin at birth (recorded at various times)
+    skin2skin_feed = letters[1:3] %>% take_sample(), # skin to skin contact until after first feed
+    skin2skin_1hr = letters[1:3] %>% take_sample(), # skin to skin contract for after one hour
+    bfeed_offer_birth = letters[1:3] %>% take_sample(),# breastfeeding offered at birth
+    bfeed_init_birth = letters[1:3] %>% take_sample(), # breastfeeding initiated at birth
+    formula_birth = letters[1:3] %>% take_sample(),# formula fed by mum at birth
+    response_parent = letters[1:3] %>% take_sample()# responsiveness for mother
+    
+  )
 
 # nimats-postnatal --------------------------------------------------------
 
+nimats_postnatal <-
+  master_id_df %>%
+  mutate(
+    smoke_12hr_m = letters %>% take_sample(), # Smoking last 12 hours 
+    smoke_postnatal_cd_m = letters %>% take_sample(),# Currently smoking code
+    smoke_postnatal_m = letters %>% take_sample(), # Currently smoking description
+    cigs_n_m = 1:10 %>% take_sample(), # Number of cigaretters smoked per day
+    smoke_refer_m =  letters %>% take_sample(), # Refer to smoking cessation
+    smoke_refer_acpt_m = letters %>% take_sample(), # Referal accepted 
+    meds_m = letters %>% take_sample() # Uptake of maternal medication (vitamins, folic acid, iron)
+    
+  )
 
 
+# nimats_refer ------------------------------------------------------------
+nimats_refer <-
+  master_id_df %>%
+  mutate(
+    grfb_refer = letters %>% take_sample(), # Getting Ready For Baby (GRFB) suitable/offered/ accepted/ completed
+    stop_smoke_refer = letters %>% take_sample()# Attend stop smoking service
+    
+  )
 
+
+# save outputs ------------------------------------------------------------
+nimats_bfeed %>% write_csv('outputs/nimats-bfeed.csv')
+nimats_delivery %>% write_csv('outputs/nimats-delivery.csv')
+nimats_infant_df %>% write_csv('outputs/nimats-infant.csv')
+nimats_main_df %>% write_csv('outputs/nimats-main.csv')
+nimats_obst_df %>% write_csv('outputs/nimats-obst.csv')
+nimats_parenting %>% write_csv('outputs/nimats-parenting.csv')
+nimats_postnatal %>% write_csv('outputs/nimats-postnatal.csv')
+nimats_refer %>% write_csv('outputs/nimats-refer.csv')
+
+## save a combined file
+master_id_df %>%
+  left_join(nimats_bfeed) %>%
+  left_join(nimats_delivery) %>%
+  left_join(nimats_infant_df) %>%
+  left_join(nimats_main_df) %>%
+  left_join(nimats_obst_df) %>%
+  left_join(nimats_parenting) %>%
+  left_join(nimats_postnatal) %>%
+  left_join(nimats_refer) %>%
+  write_csv('outputs/nimats-all.csv')
